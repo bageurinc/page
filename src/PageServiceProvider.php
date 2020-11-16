f9fc038b3b0a3616c07c639f05a53c835bdb9637
ariqamuharam <ariqamuharam@gmail.com>
2020-11-16 17:33:47 +0700

    package page

---
+++ src/PageServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bageur\Page;
+
+use Illuminate\Support\ServiceProvider;
+
+class PageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // include __DIR__.'/routes/web.php';
+        $this->app->make('Bageur\Page\PageCmsController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/migration');
+    }
+}
---
+++ src/model/page.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bageur\page\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class page extends Model
+{
+    protected $table = 'bgr_page';
+
+    public function scopeDatatable($query,$request,$page=12)
+    {
+          $search       = ["judul"];
+          $searchqry    = '';
+
+        $searchqry = "(";
+        foreach ($search as $key => $value) {
+            if($key == 0){
+                $searchqry .= "lower($value) like '%".strtolower($request->search)."%'";
+            }else{
+                $searchqry .= "OR lower($value) like '%".strtolower($request->search)."%'";
+            }
+        }
+        $searchqry .= ")";
+        if(@$request->sort_by){
+            if(@$request->sort_by != null){
+            	$explode = explode('.', $request->sort_by);
+                 $query->orderBy($explode[0],$explode[1]);
+            }else{
+                  $query->orderBy('created_at','desc');
+            }
+
+             $query->whereRaw($searchqry);
+        }else{
+             $query->whereRaw($searchqry);
+        }
+        if($request->get == 'all'){
+            return $query->get();
+        }else{
+                return $query->paginate($page);
+        }
+
+    }
+}
